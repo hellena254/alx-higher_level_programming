6f64ba74df943afcb4089c3570c2ba4ef989b782
hellenah254 <hellenah243@gmail.com>
2023-12-07 04:58:24 +0300

    more data structures

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    m = list(map(list, zip(*my_list)))
+    n = [x * y for x, y in zip(m[0], m[1])]
+    return sum(n) / sum(m[1])
