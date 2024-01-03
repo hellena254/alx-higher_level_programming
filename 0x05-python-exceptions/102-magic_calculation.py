2b2b7736b385763c299863295b3d276e7c764176
Hellen <hellenah243@gmail.com>
2024-01-03 13:41:03 +0000

    translating a bytecode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     res = 0
-    for i in range(1, 4):
+    for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
-            res += (a ** b) / 1
+            res += (a ** b) / i
         except Exception:
-            res += b + a
+            res = b + a
             break
 
     return res
