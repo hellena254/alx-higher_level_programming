2af8ae895ee73caf3ce35640f9387e08747abb7b
Hellen <hellenah243@gmail.com>
2024-01-04 11:42:44 +0000

    task 4 and 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -17,7 +17,12 @@ class Square:
         TypeError: If size is not an integer.
         ValueError: If size is less than 0.
         """
-        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >- 0")
+        else:
+            self.__size = size
 
     @property
     def size(self):
