7c71c793043f1cc83eed8bfaea67c67ecbdbd109
Hellen <hellenah243@gmail.com>
2024-01-04 10:38:01 +0000

    task 0 and 1

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-square.py
+"""Defines an empty class"""
+
+
+class Square:
+    """Does nothing!"""
+
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 1-square.py
+"""A class square"""
+
+
+class Square:
+    """Has a private instance attribute"""
+
+    def __init__(self, size):
+        """instantianted with size"""
+
+        self.__size = size
