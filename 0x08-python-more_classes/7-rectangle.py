c4a775c69db800f8c2278275b9dc10d33542e92f
Hellen <hellenah243@gmail.com>
2024-01-05 09:54:08 +0000

    7-rectangle

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -87,4 +87,4 @@ class Rectangle:
     def __del__(self):
         """when an instance is deleted"""
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
+        type(self).number_of_instances -= 1
