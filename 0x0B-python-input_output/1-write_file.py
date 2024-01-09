d33f0a128062605d4679a478b0fb023b292f80f6
Hellen <hellenah243@gmail.com>
2024-01-09 12:31:51 +0000

    file input/output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-"""a function that reads a text file"""
+'''Define write a string to a text file function.'''
 
 
 def write_file(filename="", text=""):
-    """function that reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, "w", encoding="utf-8") as files:
-        print(files.write()
+    '''Write a string to a UTF-8 text file.
+
+    Args:
+       filename (str): The name of the file to write.
+       text (str): The text to write to the file.
+    Returns:
+       The number of characters written.
+    '''
+    with open(filename, "w", encoding="utf-8") as myfile:
+        return (myfile.write(text))
