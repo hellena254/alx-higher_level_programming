64f9206f18951b8302889988e0bad1468e4283a3
Hellen <hellenah243@gmail.com>
2024-01-10 08:11:18 +0000

    file input/output

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""add all arguments to a Python list"""
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+import sys
+
+try:
+    data = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    data = []
+
+new_data = data + sys.argv[1:]
+# save the updated data
+save_to_json_file(new_data, "add_item.json")
