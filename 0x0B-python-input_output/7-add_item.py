8a5f535f5450638058a39891feb77c5494565e01
Hellen <hellenah243@gmail.com>
2024-01-10 08:22:10 +0000

    file input/output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """add all arguments to a Python list"""
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import sys
 
-try:
-    data = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    data = []
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-data.extend(sys.argv[1:])
-# save the updated data
-save_to_json_file(data, "add_item.json")
+    try:
+        data = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        data = []
+
+    data.extend(sys.argv[1:])
+    # save the updated data
+    save_to_json_file(data, "add_item.json")
