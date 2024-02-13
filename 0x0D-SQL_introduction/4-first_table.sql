e1485b873c94997726ca14e3e4f392b055727cb9
hellena254 <hellenah243@gmail.com>
2024-02-13 05:33:50 -0800

    create first_table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- create table first_table if it does not exist
 -- has id: int and name: varchar
-CREATE TABLE `first_table`(
-		`id` INT,
-		`name` VARCHAR(256)
+CREATE TABLE first_table(
+		id INT,
+		name VARCHAR(256)
 		);
