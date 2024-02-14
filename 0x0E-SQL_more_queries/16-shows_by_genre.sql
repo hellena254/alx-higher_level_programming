cb7116af1f379d60493b8f6bfa8a9aca07278aaa
hellena254 <hellenah243@gmail.com>
2024-02-14 10:28:30 -0800

    list all genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- script that lists all shows and all genres linked to that show
-SELECT tv_shows.title, COALESCE(tv_genres.name, 'NULL') AS genre_name
+SELECT tv_shows.title, name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, genre_name ASC;
+ORDER BY tv_shows.title ASC, name ASC;
 
