17dbfc811af50cf81aabbee7102afb5bdc5fc3ac
hellena254 <hellenah243@gmail.com>
2024-02-14 09:53:15 -0800

    list all tv shows

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,7 @@
 -- Import the database dump from hbtn_0d_tvshows
 --  lists all shows contained in hbtn_0d_tvshows
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
