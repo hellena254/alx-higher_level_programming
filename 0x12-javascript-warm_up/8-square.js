837efb5bc54e6ee1f58d8ccd06f7d8a57dacc454
hellena254 <hellenah243@gmail.com>
2024-03-12 03:19:56 -0700

    print a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,10 +2,10 @@
 if (isNaN(process.argv[2]) || process.argv === undefined) {
   console.log('Missing size');
 } else {
-    const x = Number(process.argv[2]);
-    let i = 0;
-    while (i < x) {
-      console.log('x'.repeat(x));
-      i++;
-    }
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('x'.repeat(x));
+    i++;
+  }
 }
