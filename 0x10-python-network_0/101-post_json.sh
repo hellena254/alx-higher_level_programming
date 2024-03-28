e024eb0ad8e29569fbb71d6229fc85172f7bc3e3
hellena254 <hellenah243@gmail.com>
2024-03-28 06:37:28 -0700

    advanced tasks

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# sends a JSON POST request to a URL passed as the first argument
+# displays the body of the response
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
