0d3718cb3e492ec952fa86762b7c37315abc7739
hellena254 <hellenah243@gmail.com>
2024-04-01 01:33:31 -0700

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://alx-intranet.hbtn.io/status
+A Python script that fetches https://alx-intranet.hbtn.io/status
 """
 
 import urllib.request
@@ -8,6 +8,6 @@ import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
     print("Body response:")
-    print("\t- type: ", type(content))
-    print("\t- content: ", content)
-    print("\t- utf8 content: ", content.decode('utf-8'))
+    print("\t- type: ", type(html))
+    print("\t- content: ", html)
+    print("\t- utf8 content: ", html.decode('utf-8'))
