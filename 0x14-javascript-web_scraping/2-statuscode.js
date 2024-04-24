dd898239234055a945400049b7ae80e0f4bb56c0
hellena254 <hellenah243@gmail.com>
2024-04-23 23:25:46 -0700

    GET status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,8 +6,7 @@ const url = process.argv[2];
 
 request(url, (error, response) => {
   if (error) {
-    console.error(error);
+    console.log(error);
   }
   console.log(`code: ${response.statusCode}`);
 });
-
